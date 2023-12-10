d397bd85ddbf63f8186fb4bc96243a78e2be1a13
Бексултан Өмірзақ <beksultanomirzak.03@gmail.com>
2023-12-10 20:07:27 +0600

    убрал  Bridge

--- src/pages/Transfer/Transfer.jsx
+++ src/pages/Transfer/Transfer.jsx
@@ -25,11 +25,11 @@ function Transfer() {
 
   const handleSubmitBtn=()=>{
     console.log('Bridge')
-   setTimeout(()=>{
-    Bridge.interfaces.GreenWallet.connect().then((ans) => {
-      console.log(ans,'Call from Android for checking connection');
-  });
-   },50)
+  //  setTimeout(()=>{
+  //   Bridge.interfaces.GreenWallet.connect().then((ans) => {
+  //     console.log(ans,'Call from Android for checking connection');
+  // });
+  //  },50)
   }
 
 
